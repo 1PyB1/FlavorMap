e3027a7896c875e54ba032f31d6bafff1d56e003
halilcode <halilmetin1530@gmail.com>
2026-03-25 23:03:33 +0300

    update filter

--- Core/views.py
+++ Core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Location, Restaurant
+from .models import Location, Restaurant, Category
 from django.core.paginator import Paginator
 
 def home_page(request):
@@ -10,15 +10,28 @@ def profile_page(request):
     return render(request, template_name= "core/profile.html")
 def restaurants_page(request):
     restaurants = Restaurant.objects.all()
+    categories = Category.objects.all()
 
-    paginator = Paginator(restaurants, 9) 
+    category_id = request.GET.get('category')
+    search = request.GET.get('search')
 
+    if category_id:
+        restaurants = restaurants.filter(categories=category_id)
+
+    if search:
+        restaurants = restaurants.filter(name__icontains=search)
+
+    if search and category_id:
+        restaurants = restaurants.filter(categories=category_id, name__icontains=search)
+
+    paginator = Paginator(restaurants, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'core/restaurants.html', {
-        'page_obj': page_obj
-        })
+        'page_obj': page_obj,
+        'categories': categories,
+    })
 def restaurant_detail(request, id):
     restaurant = Restaurant.objects.get(id=id)
 
